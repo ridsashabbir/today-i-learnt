9c9badff8862cd6272784e4a333d77a5ee5fd21c
ridsashabbir <ridsashabbir123@gmail.com>
2024-01-24 23:24:10 +0500

    time complexity of move zeroes at the end of array

--- interview-prep-coding/shift-zeroes.js
+++ interview-prep-coding/shift-zeroes.js
@@ -12,3 +12,14 @@ const shiftZeroes = (arr) => {
 let arr = [0, 1, 0, 2, 3, 0, 0, 0, 4, 0, 5, 0];
 let result = shiftZeroes(arr);
 console.log(result);
+
+// O(n^2)
+
+// The time complexity of the provided `shiftZeroes` function is O(n^2), where n is the length of the input array.
+
+// Explanation:
+
+// 1. **Looping through Array:** The for loop iterates through each element of the array once, resulting in O(n) time complexity.
+// 2. **Splicing and Pushing:** Within the loop, the `splice` and `push` operations are performed. The `splice` operation has an average time complexity of O(n), where n is the length of the array. Since it is performed inside a loop, the overall time complexity becomes O(n^2).
+// So, combining both the array iteration and the splicing/pushing operations, the overall time complexity of the `shiftZeroes` function is O(n^2).
+// It's worth noting that the repeated use of `splice` within the loop contributes significantly to the time complexity. If you want to optimize the solution, consider using a two-pointer approach, which would bring the time complexity down to O(n).
