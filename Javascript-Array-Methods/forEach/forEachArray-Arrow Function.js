ee3f6f462181b803799973e0c2b57c13db3e6a37
ridsashabbir <ridsashabbir123@gmail.com>
2024-02-20 23:34:46 +0500

    reason for arrow function

--- Javascript-Array-Methods/forEach/forEachArray-Arrow Function.js
+++ Javascript-Array-Methods/forEach/forEachArray-Arrow Function.js
@@ -8,3 +8,9 @@ numbers.forEach((item, index, arr) => {
   sum += item;
 });
 console.log("Sum of elements of array is: ", sum);
+
+// Arrow functions don't have their own arguments object like regular
+// functions. They lexically capture the arguments of their containing scope.
+//  So, even if you don't use all the parameters provided in the arrow
+//  function, they are still defined and accessible within the function
+//  body.
