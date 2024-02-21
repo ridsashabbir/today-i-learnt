ba5939b0f644c6b7d67bbe472ec32c5979428eb4
ridsashabbir <ridsashabbir123@gmail.com>
2024-02-21 23:32:43 +0500

    forEach Regular Function

---
+++ Javascript-Array-Methods/forEach-Regular Function.js
@@ -0,0 +1,19 @@
+// call forEach method for three functions separately
+numbers.forEach(myfuntion1);
+numbers.forEach(myfuntion2);
+numbers.forEach(myfuntion3);
+
+// it will utilize all the 3 arguments
+function myfuntion1(item, index, arr) {
+  console.log((arr[index] = item * 10));
+}
+
+// it will utlize only two arguments and will still work fine
+function myfuntion2(item, index, arr) {
+  console.log(arr[index] + 2);
+}
+
+// if we will not pass three arguments it will throw error of undefined
+function myfuntion3(index, arr) {
+  console.log(arr[index]);
+}
